e504e09170718c58cf566be9dce19bd5897edad5
Fael <faelazomoreno@gmail.com>
2011-11-08 13:20:38 +0100

    Mejorada la Interfa de Cliente, construida la seccion Plantillas.

---
+++ PruebaPaneles.Web/js/prueba.js
@@ -0,0 +1,36 @@
+﻿jQuery(document).ready(function () {
+
+    jQuery('#tabs').tabs();
+
+    jQuery('#exit').dialog({
+        modal: true,
+        resizable: false,
+        width: 300,
+        autoOpen: false,
+        buttons: {
+            Ok: function () {
+
+                jQuery(location).attr("href", "login.htm");
+            },
+            Cancel: function () {
+
+                jQuery(this).dialog("close");
+            }
+        }
+    });
+
+    jQuery('#btLogout').button();
+
+    jQuery('#btLogout').click(function () {
+        
+        jQuery('#exit').dialog("open");
+    });
+
+    
+
+});
+
+function pintar() {
+    alert('Si entra');
+    jQuery('#lbUser').css("background-color", "red");
+}
